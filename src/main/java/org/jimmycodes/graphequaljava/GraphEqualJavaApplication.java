2873c02268a924f7469af74167ed21edc4d7c73a
Jimmy North <jimnorth1982@gmail.com>
2026-02-08 09:34:05 -0500

    Init structure

---
+++ src/main/java/org/jimmycodes/graphequaljava/GraphEqualJavaApplication.java
@@ -0,0 +1,13 @@
+package org.jimmycodes.graphequaljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphEqualJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphEqualJavaApplication.class, args);
+    }
+
+}
