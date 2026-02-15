66ab9d2b3154e2695b1be65ca02cf8a3bfce2d54
Jimmy North <jimnorth1982@gmail.com>
2026-02-15 16:56:58 -0500

    Code formatter

--- graph-equal-web/src/app/app.config.ts
+++ graph-equal-web/src/app/app.config.ts
@@ -1,11 +1,26 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, inject, provideBrowserGlobalErrorListeners} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {provideHttpClient} from '@angular/common/http';
+import {provideApollo} from 'apollo-angular';
+import {HttpLink} from 'apollo-angular/http';
+import {InMemoryCache} from '@apollo/client';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideApollo(() => {
+      const httpLink = inject(HttpLink);
+
+      return {
+        link: httpLink.create({
+          uri: 'http://localhost:8080',
+        }),
+        cache: new InMemoryCache(),
+      };
+    })
   ]
 };
--- graph-equal-web/src/app/user.ts
+++ graph-equal-web/src/app/user.ts
@@ -1,8 +1,42 @@
 import { Injectable } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class User {
-  
+  private PATH = "/graphql";
+  private HOST = "http://localhost:4000";
+  private fullUrl = this.HOST + this.PATH;
+
+  constructor(private apollo: Apollo) { }
+
+  getUsers(): Observable<any> {
+    return this.apollo
+      .watchQuery({
+        query: gql`
+          query {
+            users {
+              id
+              firstName
+              lastName
+              email
+              orders {
+                id
+                userId
+                product
+                __typename
+              }
+              __typename
+            }
+          }
+        `,
+        context: {
+          uri: this.fullUrl,
+        },
+      })
+      .valueChanges.pipe(map((result: any) => result.data.users));
+  }
 }
